60149d17c8083e75c3dbb60ee820e5d75f3b53e3
happyrunner810 <65887114+happyrunner810@users.noreply.github.com>
2023-12-15 21:11:29 +0200

    Add files via upload

---
+++ project/js/data.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("loaded page")
+    var list = document.getElementById(""); //change this to get the element needed
+
+    var card = " ";
+    list.insertAdjacentHTML("beforeend", card);
+});
+
+function addToCart() {
+    console.log("hello");
+}
